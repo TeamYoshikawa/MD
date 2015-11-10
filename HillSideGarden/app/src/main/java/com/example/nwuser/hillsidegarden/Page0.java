9e4a2dd35dcd4669dd831dbd157968c357d1e04a
ydakoukn <ydakoukun0105@gmail.com>
2015-11-11 05:23:50 +0900

    とりあえず追加

---
+++ HillSideGarden/app/src/main/java/com/example/nwuser/hillsidegarden/Page0.java
@@ -0,0 +1,19 @@
+package com.example.nwuser.hillsidegarden;
+
+/**
+ * Created by nwuser on 2015/11/11.
+ */
+
+import com.example.nwuser.hillsidegarden.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class Page0 extends PageBase{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        return inflater.inflate(R.layout.page0,null);
+    }
+}
---
+++ HillSideGarden/app/src/main/java/com/example/nwuser/hillsidegarden/PageBase.java
@@ -0,0 +1,9 @@
+package com.example.nwuser.hillsidegarden;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by nwuser on 2015/11/11.
+ */
+public class PageBase extends Fragment {
+}
